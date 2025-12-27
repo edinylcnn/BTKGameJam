2ba6190b41786f5562b64473e19742ded3136092
Edin <edinylcnn@gmail.com>
2025-12-27 16:48:31 +0300

    Asansör kapı açılıp kapanma

--- Assets/Scripts/ElevatorController.cs
+++ Assets/Scripts/ElevatorController.cs
@@ -12,17 +12,31 @@ public class ElevatorController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        OpenDoor();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            OpenDoor();
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            CloseDoor();
+        }
     }
 
     public void OpenDoor()
     {
-        RgtDoor.DOLocalMoveX(-2, 0.6f);
-        LftDoor.DOLocalMoveX(2, 0.6f);
+        RgtDoor.DOLocalMoveX(-2, 0.6f).SetEase(Ease.OutQuad);
+        LftDoor.DOLocalMoveX(2, 0.6f).SetEase(Ease.OutQuad);
+    }
+
+    public void CloseDoor()
+    {
+        RgtDoor.DOLocalMoveX(-0.8f, 0.6f).SetEase(Ease.OutQuad);
+        LftDoor.DOLocalMoveX(0.8f, 0.6f).SetEase(Ease.OutQuad);
     }
 }
