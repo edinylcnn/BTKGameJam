6ea9058849c03f78173d253a4757b7415e73507c
yusufcyhn <yusufceyhan11@outlook.com>
2025-12-28 14:41:17 +0300

    Unity değişiklikleri eklendi son(yusuf)

---
+++ Assets/Scripts/SO/YusufScripts/GameMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameMenu : MonoBehaviour
+{
+
+    public Button startButton;
+    public Button endButton;
+
+    public void Start()
+    {
+        startButton.onClick.AddListener(NewGame);
+        endButton.onClick.AddListener(ExitGame);
+    }
+    public void NewGame()
+    {
+        Debug.Log("buraya girdi mi");
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Oyun kapatýlýyor...");
+        Application.Quit();
+    }
+}
